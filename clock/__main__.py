9831e6085653eabf2cbc98d2f565f24ce768825c
Dan Dawson <randomsilo@gmail.com>
2017-10-09 14:26:33 -0500

    added raleway font

--- clock/__main__.py
+++ clock/__main__.py
@@ -1,4 +1,12 @@
 from kivy.app import App
+from kivy.core.text import LabelBase
+
+LabelBase.register(name="Raleway",
+    fn_regular="Raleway-Regular.ttf",
+    fn_bold="Raleway-Bold.ttf",
+    fn_italic="Raleway-Italic.ttf",
+    fn_bolditalic="Raleway-BoldItalic.ttf")
+
 
 class ClockApp(App):
     pass
