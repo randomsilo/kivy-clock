18c27647aa15694fad729d6d5c706ccb3f24e4bc
Dan Dawson <randomsilo@gmail.com>
2017-10-09 15:47:49 -0500

    added stop watch

--- clock/__main__.py
+++ clock/__main__.py
@@ -5,6 +5,8 @@ from kivy.core.text import LabelBase
 from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
 from kivy.clock import Clock
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
 
 
 LabelBase.register(name="Aller",
@@ -13,13 +15,36 @@ LabelBase.register(name="Aller",
     fn_italic="Aller_It.ttf",
     fn_bolditalic="Aller_BdIt.ttf")
 
-
 class ClockApp(App):
-    def update_time(self, nap):
-        self.root.ids.timeLabel.text = datetime.now().strftime('[b]%H[/b]:%M:%S')
+    sw_started = False
+    sw_seconds = 0
 
     def on_start(self):
-        Clock.schedule_interval(self.update_time, 1)
+        Clock.schedule_interval(self.update, 0)
+
+    def update(self, nap):
+        if self.sw_started:
+            self.sw_seconds += nap
+
+        self.root.ids.time_label.text = datetime.now().strftime('[b]%H[/b]:%M:%S')
+
+        m, s = divmod(self.sw_seconds, 60)
+        self.root.ids.stopwatch.text = ('%02d:%02d.[size=40]%02d[/size]' %
+                                        (int(m), int(s), int(s * 100 % 100)))
+
+    def start_stop(self):
+        self.root.ids.start_stop.text = 'Start' if self.sw_started else 'Stop'
+        self.sw_started = not self.sw_started
+
+    def reset(self):
+        if self.sw_started:
+            self.root.ids.start_stop.text = 'Start'
+            self.sw_started = False
+
+        self.sw_seconds = 0
+
+    def on_start(self):
+        Clock.schedule_interval(self.update, 1)
 
 if __name__ == '__main__':
     Window.clearcolor = get_color_from_hex('#101216')
